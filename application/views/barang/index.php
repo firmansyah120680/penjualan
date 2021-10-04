64194d0ad227fbce4deccbe6aebe0e733c0f4705
firmansyah120680 <firmansyahgro@gamil.com>
2021-10-04 22:00:46 +0700

    Upload Awal Project

--- application/views/barang/index.php
+++ application/views/barang/index.php
@@ -1,4 +1,11 @@
 <div class="container">
+  <?php if ($this->session->flashdata('flash')) : ?>
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      <strong>Data Barang</strong> <?= $this->session->flashdata('flash'); ?>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+
+  <?php endif; ?>
   <div class="row-mt-3">
     <div class="col md-6">
       <a href="" class="btn btn=primary">Tambah Data Barang</a>
@@ -11,6 +18,7 @@
         <th scope="col">Nama Barang</th>
         <th scope="col">Harga</th>
         <th scope="col">Stok</th>
+        <th scope="col">Action</th>
       </tr>
     </thead>
     <tbody>
@@ -20,10 +28,12 @@
           <td><?= $brg['nama_barang']; ?></td>
           <td><?= $brg['harga']; ?></td>
           <td><?= $brg['stok']; ?></td>
+          <td><a href="<?= base_url(); ?>barang/detail/<?= $brg['id_barang']; ?>" class="btn btn-primary">Detail</a>
+            <a href="<?= base_url(); ?>barang/ubah/<?= $brg['id_barang']; ?>" class="btn btn-success">Ubah</a>
+            <a href="<?= base_url(); ?>barang/hapus/<?= $brg['id_barang']; ?>" class="btn btn-danger" onclick="return confirm ('Apakah Yakin Anda Dihapus?')">Hapus</a>
+          </td>
       </tr>
     <?php endforeach ?>
-
-
     </tbody>
   </table>
 </div>
